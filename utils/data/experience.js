095ca783dad97b34526d578249f5d9acedad6b9d
MyTution <rohitsonawane889@gmail.com>
2025-02-06 10:44:27 +0530

    portfolio

---
+++ utils/data/experience.js
@@ -0,0 +1,39 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'React Developer',
+    company: "LockTrust",
+    duration: "(Oct 2024 - Present)",
+    tools: ['React JS', 'Javascript', 'Reactstrap', 'Postman'],
+    image: '/png/Lt.webp',
+    description: 'Developed a chatbot using Python and Dialogflow for internal use, improving onboarding and training efficiency by 20%. Designed and implemented automation solutions for Trello boards to streamline task management, resulting in a 35% increase in efficiency.'
+  },
+  {
+    id: 2,
+    title: "Frontend developer And Java ",
+    company: "softcrowd technologies ",
+    duration: "(April 2024 - Oct 2024)",
+    tools: ['React Js , HTML/CSS , Javascript , Java'],
+    image:  '/png/sc.jpeg',
+    description: 'Managed 30+ datasets for the Center for Women in Tech assessment, generating 10+ reports and 30+ visualizations using Tableau and Power BI. Mentored 50 students in Natural Language Processing, providing tutoring and feedback. Employed advanced Excel functions and Python for ETL processes and data visualization.'
+  },
+  {
+    id: 3,
+    title: 'Data Science And Machine Learning',
+    company: "TCR Innovation",
+    duration: "(feb 2022- April 2022)",
+    tools: ['Python'],
+    image: '/png/tcr.jpeg',
+    description: 'Redesigned an internal time tracker application using ASP.NET Core MVC, improving user experience and increasing reporting efficiency by 25%. Optimized application performance with unit testing and CI/CD pipelines using Jenkins, resulting in a 15-18% increase in page load times. Utilized advanced debugging and automated testing to achieve a 20% faster page load time.'
+  },
+  // {
+  //   id: 4,
+  //   title: 'Software Engineer',
+  //   company: "La Net Team Software Solutions",
+  //   duration: "(Aug 2020- Sept 2021)",
+  //   tools: ['React Native', 'TypeScript', 'Redux', 'AWS Amplify', 'AWS CloudWatch', 'AWS Pinpoint', 'Real-time Tracking', 'Multilingual Support'],
+  //   image: '/png/lanet.jpeg',
+  //   description: 'Designed and built a ridesharing application using React Native and TypeScript, integrating AWS services like Amplify, CloudWatch, and Pinpoint for efficient data management. Developed a restaurant application with real-time order tracking, interactive menus, and multilingual support, reducing order processing time by 30%.'
+  // },
+]
+
---
+++ utils/data/projects-data.js
@@ -0,0 +1,91 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'Book Store - Frontend',
+        tools: ['React Js', 'Tailwind CSS', 'MongoDB', 'Express.Js', 'Node.Js',],
+        role: 'Frontend Developer',
+        code: '',
+        demo: 'https://youtube.com/playlist?list=PLQePrdBWxlsXQREK6iZuaEPGVOKFoVBWh&si=GTli4NfW1-7ZAo7y',
+        role: 'Full Stack Developer',
+        description: 'Engineered a dual ridesharing app using React Native, GraphQL, and AWS services, achieving 99.5% uptime. Key features include vehicle-type booking, real-time driver updates, seamless payments, a chatbot boosting engagement by 15%, trip histories, and saved locations for 25% quicker bookings.'
+    },
+    {
+        id: 2,
+        name: 'Weather App',
+        tools: ['React.js', 'Material-UI', 'Weather API', 'HTML5', 'CSS3', 'JavaScript'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Designed, developed, and deployed an e-commerce website with secure payment gateway integration, utilizing Java, MVC architecture, HTML5, CSS3, JavaScript, and third-party APIs. Containerized the application using Docker and deployed on a Kubernetes cluster for improved scalability and reliability.'
+
+    },
+    {
+        id: 3,
+        name: 'Global News Reader',
+        tools: ['React.js', 'JavaScript', 'Bootstrap'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Developed a personal finance assistant using Python and React.js that helps users manage their expenses and savings through predictive analytics and personalized insights, resulting in a 25% increase in savings over six months. Created a machine learning model leveraging historical spending data to categorize transactions and forecast monthly budgets with 90% accuracy.'
+
+    },
+    {
+        id: 4,
+        name: 'NetFlix Clone',
+        tools: ['React.js', 'JavaScript', 'Tailwind CSS'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Developed a personal finance assistant using Python and React.js that helps users manage their expenses and savings through predictive analytics and personalized insights, resulting in a 25% increase in savings over six months. Created a machine learning model leveraging historical spending data to categorize transactions and forecast monthly budgets with 90% accuracy.'
+
+    },
+    {
+        id: 5,
+        name: 'Text Converter',
+        tools: ['React JS, Bootstrap ,JavaScript'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Developed a personal finance assistant using Python and React.js that helps users manage their expenses and savings through predictive analytics and personalized insights, resulting in a 25% increase in savings over six months. Created a machine learning model leveraging historical spending data to categorize transactions and forecast monthly budgets with 90% accuracy.'
+
+    },
+    {
+        id: 6,
+        name: 'Books Collection UI',
+        tools: ['HTML/CSS'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Developed a personal finance assistant using Python and React.js that helps users manage their expenses and savings through predictive analytics and personalized insights, resulting in a 25% increase in savings over six months. Created a machine learning model leveraging historical spending data to categorize transactions and forecast monthly budgets with 90% accuracy.'
+
+    },
+    {
+        id: 7,
+        name: 'Online Course UI ',
+        tools: ['HTML/CSS','Bootstrap'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+        description: 'Developed a personal finance assistant using Python and React.js that helps users manage their expenses and savings through predictive analytics and personalized insights, resulting in a 25% increase in savings over six months. Created a machine learning model leveraging historical spending data to categorize transactions and forecast monthly budgets with 90% accuracy.'
+
+    },
+];
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
